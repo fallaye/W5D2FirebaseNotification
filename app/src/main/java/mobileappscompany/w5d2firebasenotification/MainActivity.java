1dd9a70484a741e40c939d1b53e7a3ea92bb6507
fallaye <leger.leger20@gmail.com>
2018-01-03 13:47:52 -0500

    Initial commit

---
+++ app/src/main/java/mobileappscompany/w5d2firebasenotification/MainActivity.java
@@ -0,0 +1,13 @@
+package mobileappscompany.w5d2firebasenotification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
